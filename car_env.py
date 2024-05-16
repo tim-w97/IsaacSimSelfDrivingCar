5f53c8178f0721dd303b2b3cdd74a2843f7be7af
Tim <timwagner997@gmail.com>
2024-05-16 15:12:22 +0200

    applied cartpole environment to a car

--- car_env.py
+++ car_env.py
@@ -1,24 +1,11 @@
-# TODO: this is the Cartpole example from the docs
-#  change this script so it works with our self driving car
-
-import math
-
-import numpy as np
-import torch
-from omni.isaac.core.articulations import ArticulationView
-from omni.isaac.core.utils.prims import get_prim_at_path
-from omniisaacgymenvs.tasks.base.rl_task import RLTask
-from omniisaacgymenvs.robots.articulations.cartpole import Cartpole
-
-
-class CartpoleTask(RLTask):
+class SelfDrivingCarTask(RLTask):
     def __init__(self, name, sim_config, env, offset=None) -> None:
-
         self.update_config(sim_config)
-        self._max_episode_length = 500
+        self._max_episode_length = 1000  # Adjust episode length as needed
 
-        self._num_observations = 4
-        self._num_actions = 1
+        # Update number of observations and actions based on car's state
+        self._num_observations = 8  # Example: position, velocity, orientation, etc.
+        self._num_actions = 2  # Example: steering angle, acceleration
 
         RLTask.__init__(self, name, env)
         return
@@ -30,53 +17,53 @@ class CartpoleTask(RLTask):
 
         self._num_envs = self._task_cfg["env"]["numEnvs"]
         self._env_spacing = self._task_cfg["env"]["envSpacing"]
-        self._cartpole_positions = torch.tensor([0.0, 0.0, 2.0])
+        self._car_positions = torch.tensor([0.0, 0.0, 0.0])  # Adjust initial positions
 
         self._reset_dist = self._task_cfg["env"]["resetDist"]
-        self._max_push_effort = self._task_cfg["env"]["maxEffort"]
+        self._max_steering_angle = self._task_cfg["env"]["maxSteeringAngle"]
+        self._max_acceleration = self._task_cfg["env"]["maxAcceleration"]
 
     def set_up_scene(self, scene) -> None:
-        self.get_cartpole()
+        self.get_car()
         super().set_up_scene(scene)
-        self._cartpoles = ArticulationView(
-            prim_paths_expr="/World/envs/.*/Cartpole", name="cartpole_view", reset_xform_properties=False
+        self._cars = ArticulationView(
+            prim_paths_expr="/World/envs/.*/Car", name="car_view", reset_xform_properties=False
         )
-        scene.add(self._cartpoles)
+        scene.add(self._cars)
         return
 
     def initialize_views(self, scene):
         super().initialize_views(scene)
-        if scene.object_exists("cartpole_view"):
-            scene.remove_object("cartpole_view", registry_only=True)
-        self._cartpoles = ArticulationView(
-            prim_paths_expr="/World/envs/.*/Cartpole", name="cartpole_view", reset_xform_properties=False
+        if scene.object_exists("car_view"):
+            scene.remove_object("car_view", registry_only=True)
+        self._cars = ArticulationView(
+            prim_paths_expr="/World/envs/.*/Car", name="car_view", reset_xform_properties=False
         )
-        scene.add(self._cartpoles)
+        scene.add(self._cars)
 
-    def get_cartpole(self):
-        cartpole = Cartpole(
-            prim_path=self.default_zero_env_path + "/Cartpole", name="Cartpole", translation=self._cartpole_positions
+    def get_car(self):
+        car = Car(
+            prim_path=self.default_zero_env_path + "/Car", name="Car", translation=self._car_positions
         )
-        # applies articulation settings from the task configuration yaml file
         self._sim_config.apply_articulation_settings(
-            "Cartpole", get_prim_at_path(cartpole.prim_path), self._sim_config.parse_actor_config("Cartpole")
+            "Car", get_prim_at_path(car.prim_path), self._sim_config.parse_actor_config("Car")
         )
 
     def get_observations(self) -> dict:
-        dof_pos = self._cartpoles.get_joint_positions(clone=False)
-        dof_vel = self._cartpoles.get_joint_velocities(clone=False)
+        pos = self._cars.get_joint_positions(clone=False)
+        vel = self._cars.get_joint_velocities(clone=False)
 
-        self.cart_pos = dof_pos[:, self._cart_dof_idx]
-        self.cart_vel = dof_vel[:, self._cart_dof_idx]
-        self.pole_pos = dof_pos[:, self._pole_dof_idx]
-        self.pole_vel = dof_vel[:, self._pole_dof_idx]
+        self.car_pos = pos[:, self._car_pos_idx]
+        self.car_vel = vel[:, self._car_vel_idx]
+        self.car_orientation = pos[:, self._car_orientation_idx]
+        self.car_steering_angle = pos[:, self._car_steering_idx]
 
-        self.obs_buf[:, 0] = self.cart_pos
-        self.obs_buf[:, 1] = self.cart_vel
-        self.obs_buf[:, 2] = self.pole_pos
-        self.obs_buf[:, 3] = self.pole_vel
+        self.obs_buf[:, 0] = self.car_pos
+        self.obs_buf[:, 1] = self.car_vel
+        self.obs_buf[:, 2] = self.car_orientation
+        self.obs_buf[:, 3] = self.car_steering_angle
 
-        observations = {self._cartpoles.name: {"obs_buf": self.obs_buf}}
+        observations = {self._cars.name: {"obs_buf": self.obs_buf}}
         return observations
 
     def pre_physics_step(self, actions) -> None:
@@ -89,50 +76,48 @@ class CartpoleTask(RLTask):
 
         actions = actions.to(self._device)
 
-        forces = torch.zeros((self._cartpoles.count, self._cartpoles.num_dof), dtype=torch.float32, device=self._device)
-        forces[:, self._cart_dof_idx] = self._max_push_effort * actions[:, 0]
+        controls = torch.zeros((self._cars.count, self._cars.num_dof), dtype=torch.float32, device=self._device)
+        controls[:, self._steering_dof_idx] = self._max_steering_angle * actions[:, 0]
+        controls[:, self._acceleration_dof_idx] = self._max_acceleration * actions[:, 1]
 
-        indices = torch.arange(self._cartpoles.count, dtype=torch.int32, device=self._device)
-        self._cartpoles.set_joint_efforts(forces, indices=indices)
+        indices = torch.arange(self._cars.count, dtype=torch.int32, device=self._device)
+        self._cars.set_joint_controls(controls, indices=indices)
 
     def reset_idx(self, env_ids):
         num_resets = len(env_ids)
 
-        # randomize DOF positions
-        dof_pos = torch.zeros((num_resets, self._cartpoles.num_dof), device=self._device)
-        dof_pos[:, self._cart_dof_idx] = 1.0 * (1.0 - 2.0 * torch.rand(num_resets, device=self._device))
-        dof_pos[:, self._pole_dof_idx] = 0.125 * math.pi * (1.0 - 2.0 * torch.rand(num_resets, device=self._device))
+        pos = torch.zeros((num_resets, self._cars.num_dof), device=self._device)
+        pos[:, self._car_pos_idx] = 1.0 * (1.0 - 2.0 * torch.rand(num_resets, device=self._device))
+        pos[:, self._car_orientation_idx] = 0.125 * math.pi * (1.0 - 2.0 * torch.rand(num_resets, device=self._device))
 
-        # randomize DOF velocities
-        dof_vel = torch.zeros((num_resets, self._cartpoles.num_dof), device=self._device)
-        dof_vel[:, self._cart_dof_idx] = 0.5 * (1.0 - 2.0 * torch.rand(num_resets, device=self._device))
-        dof_vel[:, self._pole_dof_idx] = 0.25 * math.pi * (1.0 - 2.0 * torch.rand(num_resets, device=self._device))
+        vel = torch.zeros((num_resets, self._cars.num_dof), device=self._device)
+        vel[:, self._car_vel_idx] = 0.5 * (1.0 - 2.0 * torch.rand(num_resets, device=self._device))
 
-        # apply resets
         indices = env_ids.to(dtype=torch.int32)
-        self._cartpoles.set_joint_positions(dof_pos, indices=indices)
-        self._cartpoles.set_joint_velocities(dof_vel, indices=indices)
+        self._cars.set_joint_positions(pos, indices=indices)
+        self._cars.set_joint_velocities(vel, indices=indices)
 
-        # bookkeeping
         self.reset_buf[env_ids] = 0
         self.progress_buf[env_ids] = 0
 
     def post_reset(self):
-        self._cart_dof_idx = self._cartpoles.get_dof_index("cartJoint")
-        self._pole_dof_idx = self._cartpoles.get_dof_index("poleJoint")
-        # randomize all envs
-        indices = torch.arange(self._cartpoles.count, dtype=torch.int64, device=self._device)
+        self._car_pos_idx = self._cars.get_dof_index("carPosition")
+        self._car_vel_idx = self._cars.get_dof_index("carVelocity")
+        self._car_orientation_idx = self._cars.get_dof_index("carOrientation")
+        self._car_steering_idx = self._cars.get_dof_index("steeringJoint")
+        self._acceleration_dof_idx = self._cars.get_dof_index("accelerationJoint")
+
+        indices = torch.arange(self._cars.count, dtype=torch.int64, device=self._device)
         self.reset_idx(indices)
 
     def calculate_metrics(self) -> None:
-        reward = 1.0 - self.pole_pos * self.pole_pos - 0.01 * torch.abs(self.cart_vel) - 0.005 * torch.abs(self.pole_vel)
-        reward = torch.where(torch.abs(self.cart_pos) > self._reset_dist, torch.ones_like(reward) * -2.0, reward)
-        reward = torch.where(torch.abs(self.pole_pos) > np.pi / 2, torch.ones_like(reward) * -2.0, reward)
+        # Example reward function for a self-driving car
+        reward = 1.0 - self.car_vel * self.car_vel - 0.01 * torch.abs(self.car_steering_angle)
+        reward = torch.where(torch.abs(self.car_pos) > self._reset_dist, torch.ones_like(reward) * -2.0, reward)
 
         self.rew_buf[:] = reward
 
     def is_done(self) -> None:
-        resets = torch.where(torch.abs(self.cart_pos) > self._reset_dist, 1, 0)
-        resets = torch.where(torch.abs(self.pole_pos) > math.pi / 2, 1, resets)
+        resets = torch.where(torch.abs(self.car_pos) > self._reset_dist, 1, 0)
         resets = torch.where(self.progress_buf >= self._max_episode_length, 1, resets)
         self.reset_buf[:] = resets
